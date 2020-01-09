e4aeae5672b7a0f39040e37099b67874d807ee8b
jusaclown <zjtxdy666@163.com>
2020-01-09 08:46:28 +0800

    -.-

--- BL_1001.c
+++ BL_1001.c
@@ -6,13 +6,11 @@ int main()
     i = 0;
     scanf("%d", &n);
     while(n != 1) {
-        if(n % 2 == 0) {
+        if(n % 2 == 0)
             n = n / 2;
-            ++i;    
-        } else {
+        else 
             n = (3 * n + 1) / 2;
-            ++i;
-        }  
+		++i;
     }
     printf("%d", i);
     return 0;
